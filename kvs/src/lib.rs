2f95019be5bae67808b806274f0ba4f4bcf5618e
berabulut <berabulut@gmail.com>
2024-02-18 17:10:20 +0000

    Refactor and more sophisticated compaction

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,3 +1,4 @@
+use chrono::prelude::*;
 use failure::{Error, Fail};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
@@ -8,10 +9,13 @@ use std::io::BufWriter;
 use std::io::{BufRead, BufReader, Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
-pub struct KvStore {
-    map: HashMap<String, u64>,
-    log_writer: BufWriter<File>,
-    log_reader: BufReader<File>,
+const COMPACTION_THRESHOLD: usize = 1024 * 1024;
+const LOG_FILE_PREFIX: &str = "kvlog";
+const LOG_FILE_EXTENSION: &str = "cmdlog";
+
+struct LogPosition {
+    pos: u64,
+    log_file_name: String,
 }
 
 pub type CommandResult<T> = Result<T, Error>;
@@ -30,6 +34,12 @@ pub enum KvSError {
     KeyNotFound,
 }
 
+pub struct KvStore {
+    key_dir: KeyDir,
+    writer_pool: WriterPool,
+    reader_pool: ReaderPool,
+}
+
 impl KvStore {
     pub fn open(path: impl Into<PathBuf>) -> CommandResult<KvStore> {
         let path = path.into();
@@ -38,75 +48,24 @@ impl KvStore {
         fs::create_dir_all(&path)?;
 
         // Initialize map with command logs from previous sessions
-        let map = init_map_with_command_logs(&path);
-
-        let log_writer = BufWriter::new(
-            OpenOptions::new()
-                .write(true)
-                .create(true)
-                .append(true)
-                .open(path.join("kvlog.cmdlog"))
-                .unwrap(),
-        );
-
-        let log_reader = BufReader::new(
-            OpenOptions::new()
-                .read(true)
-                .open(path.join("kvlog.cmdlog"))
-                .unwrap(),
-        );
+        let key_dir = KeyDir::init_with_command_logs(&path);
+        let writer_pool = WriterPool::new(&path);
+        let reader_pool = ReaderPool::new(&path);
 
         Ok(KvStore {
-            map: map,
-            log_writer,
-            log_reader,
+            key_dir,
+            writer_pool,
+            reader_pool,
         })
     }
 
-    fn write_command_log(&mut self, command_log: CommandLog) -> Result<u64, Error> {
-        let serialized_log = serde_json::to_string(&command_log)?;
-        let log_len = serialized_log.len();
-
-        writeln!(&mut self.log_writer, "{}", serialized_log)?;
-
-        let pos = self.log_writer.stream_position()? - log_len as u64 - 1;
-
-        Ok(pos)
-    }
-
-    fn read_from_pos_to_eol(&mut self, pos: u64) -> Result<String, Error> {
-        self.log_writer.flush()?;
-
-        self.log_reader.seek(SeekFrom::Start(pos))?;
-
-        let mut line = String::new();
-
-        // Read characters until the newline is found:
-        loop {
-            let mut buf = [0; 1]; // Buffer to hold a single character
-            let bytes_read = self.log_reader.read(&mut buf)?;
-
-            if bytes_read == 0 {
-                // End of file reached
-                break;
-            }
-
-            if buf[0] == b'\n' {
-                // Newline found, end of line reached
-                break;
-            }
-
-            line.push(buf[0] as char);
-        }
-
-        Ok(line)
-    }
-
     pub fn get(&mut self, key: String) -> CommandResult<Option<String>> {
-        let res = self.map.get(&key).cloned();
+        self.writer_pool.sync()?;
+
+        let res = self.key_dir.get(&key).clone();
         match res {
-            Some(pos) => {
-                let line_res = self.read_from_pos_to_eol(pos)?;
+            Some(log_pos) => {
+                let line_res = self.reader_pool.read_from_pos_to_eol(log_pos)?;
                 let command_log: CommandLog = serde_json::from_str(&line_res)?;
                 match command_log {
                     CommandLog::Set { value, .. } => Ok(Some(value)),
@@ -127,7 +86,7 @@ impl KvStore {
             value: value.clone(),
         })?;
 
-        self.map.insert(key, pos);
+        self.key_dir.set(key, pos);
 
         Ok(())
     }
@@ -137,84 +96,337 @@ impl KvStore {
             return Err(KvSError::KeyNotProvided.into());
         }
 
-        if !self.map.contains_key(&key) {
+        if !self.key_dir.contains_key(&key) {
             return Err(KvSError::KeyNotFound.into());
         }
 
         self.write_command_log(CommandLog::Remove { key: key.clone() })?;
 
-        self.map.remove(&key);
+        self.key_dir.remove(&key);
 
         Ok(())
     }
 
-    fn create_compacted_log_file(&mut self) -> Result<(), Error> {
-        let new_log_file = OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open("kvlog_compact.cmdlog")?;
-
-        self.log_writer = BufWriter::new(new_log_file);
-
-        let keys: Vec<String> = self.map.keys().map(String::from).collect();
-
-        for key in keys {
-            let value = self.get(key.clone())?.unwrap();
-            self.write_command_log(CommandLog::Set {
-                key: key,
-                value: value,
-            })?;
+    fn write_command_log(&mut self, command_log: CommandLog) -> Result<LogPosition, Error> {
+        let serialized_log = serde_json::to_string(&command_log)?;
+        if self.writer_pool.active_size() + serialized_log.len() >= COMPACTION_THRESHOLD {
+            self.compact_log_files()?;
         }
+
+        self.writer_pool.write(serialized_log)
+    }
+
+    fn compact_log_files(&mut self) -> Result<(), Error> {
+        let reader_list = self.reader_pool.reader_list();
+
+        self.writer_pool.new_writer();
+        self.reader_pool.add_reader(self.writer_pool.curr.clone());
+
+        let mut start_pos = 0;
+
+        reader_list.iter().for_each(|file_name| {
+            let reader = self.reader_pool.get_reader(file_name.to_string());
+            let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+            for line in lines {
+                let command_log: CommandLog = serde_json::from_str(&line).unwrap();
+                let should_remove =
+                    self.should_remove_log(&command_log, file_name.clone(), start_pos);
+
+                start_pos += line.len() as u64 + 1;
+
+                if should_remove {
+                    continue;
+                }
+
+                let serialized_log = serde_json::to_string(&command_log).unwrap();
+
+                if self.writer_pool.active_size() + serialized_log.len() >= COMPACTION_THRESHOLD {
+                    self.writer_pool.new_writer();
+                    self.reader_pool.add_reader(self.writer_pool.curr.clone());
+                }
+
+                self.writer_pool.write(serialized_log).unwrap();
+            }
+        });
+
+        self.reader_pool.remove_readers(reader_list);
 
         Ok(())
     }
-}
 
-impl Drop for KvStore {
-    fn drop(&mut self) {
-        // Create compacted log file and if successful, delete old log file, and rename compacted log file to old log file name
-        if let Err(e) = self.create_compacted_log_file() {
-            eprintln!("Error creating compacted log file: {}", e);
-        } else {
-            if let Err(e) = fs::remove_file("kvlog.cmdlog") {
-                eprintln!("Error deleting old log file: {}", e);
-            }
+    fn should_remove_log(&self, log: &CommandLog, file_name: String, start_pos: u64) -> bool {
+        match log {
+            CommandLog::Set { key, .. } => {
+                if !self.key_dir.contains_key(&key) {
+                    return true;
+                }
 
-            if let Err(e) = fs::rename("kvlog_compact.cmdlog", "kvlog.cmdlog") {
-                eprintln!("Error renaming compacted log file: {}", e);
+                let log_pos = self.key_dir.get(&key).unwrap();
+                if log_pos.log_file_name != file_name {
+                    return true;
+                }
+                if log_pos.pos != start_pos {
+                    return true;
+                }
+
+                return false;
             }
+            CommandLog::Remove { key: _ } => true,
         }
     }
 }
 
-fn init_map_with_command_logs(path: impl Into<PathBuf>) -> HashMap<String, u64> {
-    let mut store = HashMap::new();
-    let log_files = list_log_files(path).unwrap();
+struct KeyDir {
+    map: HashMap<String, LogPosition>,
+}
 
-    for file in log_files {
-        let file = File::open(file).unwrap();
+impl KeyDir {
+    fn init_with_command_logs(path: impl Into<PathBuf>) -> KeyDir {
+        let mut store = HashMap::new();
+        let log_files = list_log_files(path).unwrap();
+
+        for file_path in log_files {
+            let file = File::open(file_path.clone()).unwrap();
+            let reader = BufReader::new(file);
+
+            let mut pos = 0;
+            for line in reader.lines() {
+                let line = line.unwrap();
+
+                let command_log: CommandLog = serde_json::from_str(&line).unwrap();
+                match command_log {
+                    CommandLog::Set { key, .. } => {
+                        store.insert(
+                            key,
+                            LogPosition {
+                                pos,
+                                log_file_name: file_path
+                                    .file_name()
+                                    .unwrap()
+                                    .to_str()
+                                    .unwrap()
+                                    .to_string(),
+                            },
+                        );
+                    }
+                    CommandLog::Remove { key } => {
+                        store.remove(&key);
+                    }
+                }
+
+                pos += line.len() as u64 + 1;
+            }
+        }
+
+        KeyDir { map: store }
+    }
+
+    fn get(&self, key: &str) -> Option<&LogPosition> {
+        self.map.get(key)
+    }
+
+    fn set(&mut self, key: String, log_position: LogPosition) {
+        self.map.insert(key, log_position);
+    }
+
+    fn remove(&mut self, key: &str) {
+        self.map.remove(key);
+    }
+
+    fn contains_key(&self, key: &str) -> bool {
+        self.map.contains_key(key)
+    }
+}
+
+struct WriterPool {
+    path: PathBuf,
+    writers: HashMap<String, NamedBufWriter>,
+    curr: String,
+    curr_size: usize,
+}
+
+impl WriterPool {
+    // Create hash map with writers to log files, initialized with empty log file
+    fn new(path: impl Into<PathBuf>) -> WriterPool {
+        let mut writers = HashMap::new();
+        let path = path.into();
+
+        match latest_log_file_metadata(&path) {
+            Ok((lf_name, lf_size)) => {
+                if lf_size < COMPACTION_THRESHOLD as u64 {
+                    writers.insert(lf_name.clone(), NamedBufWriter::new(&path, lf_name.clone()));
+                    return WriterPool {
+                        path,
+                        writers,
+                        curr: lf_name,
+                        curr_size: lf_size as usize,
+                    };
+                }
+            }
+            _ => {}
+        }
+
+        let new_log_file_name = new_log_file_name();
+        writers.insert(
+            new_log_file_name.clone(),
+            NamedBufWriter::new(&path, new_log_file_name.clone()),
+        );
+
+        WriterPool {
+            path,
+            writers,
+            curr: new_log_file_name,
+            curr_size: 0,
+        }
+    }
+
+    fn new_writer(&mut self) {
+        let new_log_file_name = new_log_file_name();
+        self.writers.insert(
+            new_log_file_name.clone(),
+            NamedBufWriter::new(&self.path, new_log_file_name.clone()),
+        );
+        self.curr = new_log_file_name;
+        self.curr_size = 0;
+    }
+
+    fn active_size(&self) -> usize {
+        self.curr_size
+    }
+
+    fn sync(&mut self) -> Result<(), Error> {
+        self.writers.get_mut(&self.curr).unwrap().sync()?;
+        Ok(())
+    }
+
+    fn write(&mut self, s: String) -> Result<LogPosition, Error> {
+        self.curr_size += s.len();
+        return self.writers.get_mut(&self.curr).unwrap().write(s);
+    }
+}
+
+struct ReaderPool {
+    // into pathbuf
+    path: String,
+    readers: HashMap<String, BufReader<File>>,
+}
+
+impl ReaderPool {
+    fn new(path: impl Into<PathBuf>) -> ReaderPool {
+        let path = path.into();
+
+        let mut readers = HashMap::new();
+        let log_files = list_log_files(&path).unwrap();
+
+        for file_path in log_files {
+            let file_name = file_path.file_name().unwrap().to_str().unwrap();
+            let file = File::open(file_path.clone()).unwrap();
+            let reader = BufReader::new(file);
+            readers.insert(file_name.to_string(), reader);
+        }
+
+        ReaderPool {
+            path: path.to_str().unwrap().to_string(),
+            readers: readers,
+        }
+    }
+
+    fn add_reader(&mut self, file_name: String) {
+        let file = File::open(format!("{}/{}", self.path, file_name)).unwrap();
         let reader = BufReader::new(file);
+        self.readers.insert(file_name, reader);
+    }
 
-        let mut pos = 0;
-        for line in reader.lines() {
-            let line = line.unwrap();
+    fn get_reader(&mut self, file_name: String) -> &mut BufReader<File> {
+        self.readers.get_mut(&file_name).unwrap()
+    }
 
-            let command_log: CommandLog = serde_json::from_str(&line).unwrap();
-            match command_log {
-                CommandLog::Set { key, .. } => {
-                    store.insert(key, pos);
-                }
-                CommandLog::Remove { key } => {
-                    store.remove(&key);
+    fn reader_list(&self) -> Vec<String> {
+        self.readers.keys().cloned().collect()
+    }
+
+    fn remove_readers(&mut self, file_names: Vec<String>) {
+        for file_name in file_names {
+            match fs::remove_file(format!("{}/{}", self.path, file_name)) {
+                Ok(_) => {}
+                Err(e) => {
+                    panic!("Error removing file: {}", e.to_string());
                 }
             }
 
-            pos += line.len() as u64 + 1;
+            self.readers.remove(&file_name);
         }
     }
 
-    store
+    fn read_from_pos_to_eol(&mut self, log_position: &LogPosition) -> Result<String, Error> {
+        let pos = log_position.pos;
+        let file_name = log_position.log_file_name.clone();
+
+        let reader = self.get_reader(file_name);
+
+        reader.seek(SeekFrom::Start(pos))?;
+
+        let mut line = String::new();
+
+        // Read characters until the newline is found:
+        loop {
+            let mut buf = [0; 1]; // Buffer to hold a single character
+            let bytes_read = reader.read(&mut buf)?;
+
+            if bytes_read == 0 {
+                // End of file reached
+                break;
+            }
+
+            if buf[0] == b'\n' {
+                // Newline found, end of line reached
+                break;
+            }
+
+            line.push(buf[0] as char);
+        }
+
+        Ok(line)
+    }
+}
+
+struct NamedBufWriter {
+    writer: BufWriter<File>,
+    file_name: String,
+}
+
+impl NamedBufWriter {
+    fn new(path: impl Into<PathBuf>, file_name: String) -> NamedBufWriter {
+        NamedBufWriter {
+            writer: BufWriter::new(
+                OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .append(true)
+                    .open(path.into().join(file_name.clone()))
+                    .unwrap(),
+            ),
+            file_name,
+        }
+    }
+
+    fn write(&mut self, s: String) -> Result<LogPosition, Error> {
+        let writer = &mut self.writer;
+        writeln!(writer, "{}", s)?;
+
+        let start_pos = writer.stream_position()? - s.len() as u64 - 1;
+
+        Ok(LogPosition {
+            pos: start_pos,
+            log_file_name: self.file_name.clone(),
+        })
+    }
+
+    fn sync(&mut self) -> Result<(), Error> {
+        self.writer.flush()?;
+        Ok(())
+    }
 }
 
 fn list_log_files(path: impl Into<PathBuf>) -> Result<Vec<PathBuf>, Error> {
@@ -223,8 +435,8 @@ fn list_log_files(path: impl Into<PathBuf>) -> Result<Vec<PathBuf>, Error> {
         .filter_map(|entry| entry.ok())
         .collect::<Vec<_>>();
 
-    // Return only files with extension .cmdlog
-    let log_files = entries
+    // Find files with .cmdlog extension
+    let mut log_files: Vec<_> = entries
         .iter()
         .filter(|entry| entry.path().is_file())
         .filter(|entry| {
@@ -236,5 +448,36 @@ fn list_log_files(path: impl Into<PathBuf>) -> Result<Vec<PathBuf>, Error> {
         .map(|entry| entry.path())
         .collect();
 
+    log_files.sort();
+
     Ok(log_files)
+}
+
+fn new_log_file_name() -> String {
+    format!(
+        "{}_{}.{}",
+        LOG_FILE_PREFIX,
+        Utc::now().timestamp_nanos_opt().unwrap(),
+        LOG_FILE_EXTENSION
+    )
+}
+
+fn latest_log_file_metadata(path: impl Into<PathBuf>) -> Result<(String, u64), Error> {
+    let log_files = list_log_files(path)?;
+    if log_files.is_empty() {
+        return Err(failure::err_msg("No log files found"));
+    }
+
+    let latest_log_file = log_files.last().unwrap();
+    let metadata = latest_log_file.metadata()?;
+
+    Ok((
+        latest_log_file
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string(),
+        metadata.len(),
+    ))
 }
